caf93412bd42a6e4b177f3e889241dfc5ba8c8c1
asj <asj@breakoutimage.dk>
2013-08-12 21:50:36 +0200

    Whitespace

--- Andarki.Wol/Andarki.Wol.Web/App_Start/BundleConfig.cs
+++ Andarki.Wol/Andarki.Wol.Web/App_Start/BundleConfig.cs
@@ -7,23 +7,22 @@ namespace Andarki.Wol.Web
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                    "~/Content/bootstrap.css",
+                    "~/Content/site.css"));
+
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                        "~/lib/angular/angular.js",
-                        "~/lib/angular/angular-resource.js",
-                        "~/lib/angular-ui-bootstrap/ui-bootstrap-0.4.0.js",
-                        "~/lib/angular-ui-bootstrap/ui-bootstrap-tpls-0.4.0.js"));
+                            "~/lib/angular/angular.js",
+                            "~/lib/angular/angular-resource.js",
+                            "~/lib/angular-ui-bootstrap/ui-bootstrap-0.4.0.js",
+                            "~/lib/angular-ui-bootstrap/ui-bootstrap-tpls-0.4.0.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/wol").Include(
-                 "~/js/app.js",
-                 "~/js/controllers.js",
-                 "~/js/directives.js",
-                 "~/js/filters.js",
-                 "~/js/services.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/site.css"                
-                ));
+                     "~/js/app.js",
+                     "~/js/controllers.js",
+                     "~/js/directives.js",
+                     "~/js/filters.js",
+                     "~/js/services.js"));
         }
     }
 }
